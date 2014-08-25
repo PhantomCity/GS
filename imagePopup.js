6865f87ab9d10f0ee308cbb1c65e1263f93c0034
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-25 23:39:16 +0400

    Create imagePopup.js

---
+++ imagePopup.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name	Source image redirect
+// @namespace PSVScripts
+// @description	Auto change thumbnails to image direct link
+// @version	0.0.1.1
+// @icon	  http://findicons.com/icon/download/131805/image/48/png
+// @require https://raw.githubusercontent.com/PhantomCity/ImagePopup/master/ImageLib.js
+// ==UserScript==
+
+alert('JS loaded');
