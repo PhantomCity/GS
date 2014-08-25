f5668f642e5b2e452bd248f0b6dfb7d67e0b3a8c
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-25 23:41:17 +0400

    Update imagePopup.js

--- imagePopup.js
+++ imagePopup.js
@@ -4,6 +4,5 @@
 // @description	Auto change thumbnails to image direct link
 // @version	0.0.1.1
 // @icon	  http://findicons.com/icon/download/131805/image/48/png
-// ==UserScript==
 
 alert('JS loaded');
