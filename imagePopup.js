ee62cb727055e7b87697342b319a4f2755a7ac1d
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-25 23:46:56 +0400

    Delete imagePopup.js

--- imagePopup.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name	SR
-// @namespace PSVScripts
-// @description	Auto change thumbnails to image direct link
-// @version	0.0.1.1
-// @icon	  http://findicons.com/icon/download/131805/image/48/png
-
-window.alert('JS loaded');
