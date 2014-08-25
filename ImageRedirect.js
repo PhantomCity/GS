7b65986f9d8a7b6a6cd5abc4c6cb424b94e1e8bd
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-25 22:53:26 +0400

    Update ImageRedirect.js

--- ImageRedirect.js
+++ ImageRedirect.js
@@ -2,7 +2,7 @@
 // @name	Source image redirect
 // @namespace PSVScripts
 // @description	Auto change thumbnails to image direct link
-// @version	0.0.1
+// @version	0.0.1.1
 // @icon	http://findicons.com/icon/download/131805/image/48/png
 
 // @grant	GM_registerMenuCommand
